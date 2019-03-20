2eccb35541f2924a512b760fd5e1477062150176
TCTAHKOC <tahaburak.koc@gmail.com>
2019-03-20 15:43:12 +0300

    initial commit

---
+++ src/main/java/com/burak/springBootAdmin/SpringBootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.burak.springBootAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
